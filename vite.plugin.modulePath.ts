46d438499557d2ec99bda1dbdd1ecf219f4452b1
Cole <564603061@qq.com>
2024-07-04 13:41:01 +0800

    优化代码

--- vite.plugin.modulePath.ts
+++ vite.plugin.modulePath.ts
@@ -7,69 +7,73 @@ import path from 'node:path'
 const modulePathRE = /__ELECTRON_MODULE_PATH__([\w$]+)__/g
 
 function parseRequest(id: string): any | null {
-    const search = new URL(id, 'file:').search
-    if (!search) return null
-    return Object.fromEntries(new URLSearchParams(search))
+  const search = new URL(id, 'file:').search
+  if (!search) return null
+  return Object.fromEntries(new URLSearchParams(search))
 }
 
 function toRelativePath(filename: string, importer: string): string {
-    const relPath = path.posix.relative(path.dirname(importer), filename)
-    return relPath.startsWith('.') ? relPath : `./${relPath}`
+  const relPath = path.posix.relative(path.dirname(importer), filename)
+  return relPath.startsWith('.') ? relPath : `./${relPath}`
 }
 
+/**
+ * Support the `?modulePath` search parameter for import and return the module bundle path.
+ */
 export default function modulePathPlugin(): Plugin {
-    let sourcemap: boolean | 'inline' | 'hidden' = false
-    return {
-        name: 'vite-plugin-electron:module-path',
-        apply: 'build',
-        enforce: 'pre',
-        configResolved(config): void {
-            sourcemap = config.build.sourcemap
-        },
-        resolveId(id, importer): string | void {
-            const query = parseRequest(id)
-            if (query && query.hasOwnProperty('modulePath')) {
-                return `${id}&importer=${importer}`
-            }
-        },
-        load(id): string | void {
-            const query = parseRequest(id)
-            if (query && query.hasOwnProperty('modulePath') && query.hasOwnProperty('importer')) {
-                const hash = this.emitFile({
-                    type: 'chunk',
-                    id: id.split('?')[0],
-                    importer: query.importer
-                })
-                const refId = `__ELECTRON_MODULE_PATH__${hash}__`
-                return `
-                import { join, dirname } from 'node:path'
-                import { fileURLToPath } from 'node:url'
-                export default join(dirname(fileURLToPath(import.meta.url)), ${refId})
-                `
-            }
-        },
-        renderChunk(code, chunk): { code: string, map: SourceMapInput } | null {
-            if (code.match(modulePathRE)) {
-                let match: RegExpExecArray | null
-                const s = new MagicString(code)
+  let sourcemap: boolean | 'inline' | 'hidden' = false
+  return {
+    name: 'vite-plugin-electron:module-path',
+    apply: 'build',
+    enforce: 'pre',
+    configResolved(config): void {
+      sourcemap = config.build.sourcemap
+    },
+    resolveId(id, importer): string | void {
+      const query = parseRequest(id)
+      if (query && query.hasOwnProperty('modulePath')) {
+        return `${id}&importer=${importer}`
+      }
+    },
+    load(id): string | void {
+      const query = parseRequest(id)
+      if (query && query.hasOwnProperty('modulePath') && query.hasOwnProperty('importer')) {
+        const hash = this.emitFile({
+          type: 'chunk',
+          id: id.split('?')[0],
+          importer: query.importer
+        })
+        const refId = `__ELECTRON_MODULE_PATH__${hash}__`
+        return `
+        import { join, dirname } from 'node:path'
+        import { fileURLToPath } from 'node:url'
+        export default join(dirname(fileURLToPath(import.meta.url)), ${refId})
+        `
+      }
+    },
+    renderChunk(code, chunk): { code: string, map: SourceMapInput } | null {
+      if (code.match(modulePathRE)) {
+        const s = new MagicString(code)
 
-                while ((match = modulePathRE.exec(code))) {
-                    const [full, hash] = match
-                    const filename = this.getFileName(hash)
-                    const outputFilepath = toRelativePath(filename, chunk.fileName)
-                    const replacement = `"${outputFilepath}"`
-                    s.overwrite(match.index, match.index + full.length, replacement, {
-                        contentOnly: true
-                    })
-                }
-
-                return {
-                    code: s.toString(),
-                    map: sourcemap ? s.generateMap({ hires: 'boundary' }) : null
-                }
-            }
-
-            return null
+        while (true) {
+          const match = modulePathRE.exec(code)
+          if (!match) break
+          const [full, hash] = match
+          const filename = this.getFileName(hash)
+          const scriptFilePath = toRelativePath(filename, chunk.fileName)
+          const replacement = JSON.stringify(scriptFilePath)
+          s.overwrite(match.index, match.index + full.length, replacement, {
+            contentOnly: true
+          })
         }
+
+        return {
+          code: s.toString(),
+          map: sourcemap ? s.generateMap({ hires: 'boundary' }) : null
+        }
+      }
+
+      return null
     }
+  }
 }
